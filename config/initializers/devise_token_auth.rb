4dc424aa7d2951eed28ad9a0dadfbf149e7b3b01
Senott <senott@yahoo.com>
2020-11-03 11:08:32 -0300

    Reset password email.

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -57,4 +57,6 @@ DeviseTokenAuth.setup do |config|
   # devise confirmable module. If you want to use devise confirmable module and
   # send email, set it to true. (This is a setting for compatibility)
   # config.send_confirmation_email = true
+
+  config.require_client_password_reset_token = true
 end
