c90bc47b73c19eba1bf212d50aa2a6962549732b
Senott <senott@yahoo.com>
2020-11-03 18:07:14 -0300

    Project language set to Portuguese Brazilian.

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module OneBitEcommerce
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb, yml}')] 
   end
 end
