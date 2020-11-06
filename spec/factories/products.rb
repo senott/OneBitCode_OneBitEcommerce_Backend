92d398c795bf95458e15c5f68463c8cd28b5814f
Senott <senott@yahoo.com>
2020-11-06 11:01:09 -0300

    Game model and tests and product model refactoring.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,10 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
-    price { Faker::Commerce.price(100.0..400.0) }
-    productable { nil }
+    price { Faker::Commerce.price(range: 100.0..400.0) }
+
+    after :build do |product|
+      product.productable = create(:game)
+    end
   end
 end
