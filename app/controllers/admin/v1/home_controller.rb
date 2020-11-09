a22254e34a48be78789d42e4c1977651fe97df9c
Senott <senott@yahoo.com>
2020-11-09 15:46:49 -0300

    Some code styling.

--- app/controllers/admin/v1/home_controller.rb
+++ app/controllers/admin/v1/home_controller.rb
@@ -1,7 +1,9 @@
-module Admin::V1
-  class HomeController < ApiController
-    def index
-      render json: { message: 'Uhulll!!!!!' }
+module Admin
+  module V1
+    class HomeController < ApiController
+      def index
+        render json: { message: 'Uhulll!!!!!' }
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password,
-        :password_confirmation])
-    end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+  end
 end
