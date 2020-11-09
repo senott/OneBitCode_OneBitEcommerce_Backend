cc6dac393d743a89965318aebdd070a23621cbfc
Senott <senott@yahoo.com>
2020-11-09 09:27:39 -0300

    ProductCategory model and tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   subject { build(:product) }
-  
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 
@@ -12,4 +12,7 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
 
   it { is_expected.to belong_to(:productable) }
+
+  it { is_expected.to have_many(:product_categories).dependent(:destroy) }
+  it { is_expected.to have_many(:categories).through(:product_categories) }
 end
