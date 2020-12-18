e94f818a041ce45dc7057374a9ae8902cf481203
Senott <senott@yahoo.com>
2020-12-18 10:07:15 -0300

    CORS configuration.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
+
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: %i[get post put patch delete options head],
+      expose: %w[access-token client expiry token-type uid]
   end
 end
